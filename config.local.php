fcc3e5d854891fb997e520f88f42283504469f3b
@sebaur <sebastianurzuar@gmail.com>
2024-08-26 19:53:07 -0400

    se instala .env y .gitignore

--- config.local.php
+++ config.local.php
@@ -1,9 +1,20 @@
 <?php
+// Cargar el autoload de Composer
+require 'vendor/autoload.php';
+
+// Crear una instancia de Dotenv
+use Dotenv\Dotenv;
+
+// Cargar el archivo .env
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
 // place for your configuration directives, so you can later easily update myaac
 $config['installed'] = true;
 $config['env'] = 'prod'; // dev or prod
 $config['mail_enabled'] = true;
-$config['server_path'] = '/home/joselinux/Escritorio/oldera/SERVIDOR/';
+$config['server_path'] = $_ENV['SERVER_PATH'];
 $config['mail_admin'] = 'admin@gmail.com';
 $config['mail_address'] = 'admin@gmail.com';
 $config['date_timezone'] = 'Europe/Berlin';
